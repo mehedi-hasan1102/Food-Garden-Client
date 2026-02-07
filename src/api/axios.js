ef7e37bd42a11d0df68ab04045c606a3d6c737e2
Mehedi Hasan <mehedi.hasan11023@gmail.com>
2026-02-07 23:34:33 +0600

    fix : i update section

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { auth } from '../context/firebase/firebase.config';
 
 const axiosSecure = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+  baseURL: 'https://food-garden-server-bd.vercel.app',
 });
 
 // Add a request interceptor
